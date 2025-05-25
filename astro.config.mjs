e1fb372e99119ef0a4c72e4f5dc164c411cacbbf
Theboy017 <wolfrain017@gmail.com>
2025-05-25 13:57:44 -0400

    Fix configuration by adding GitHub adapter and setting output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
+import github from '@astrojs/github';
 export default defineConfig({
     vite: {
       plugins: [tailwindcss()],
     },
+      output: 'static',
+  adapter: github(),
   });
